71a094a3260aba8891588555b5a623d60a9b8be8
Bruno <Bruno@BrunoPC.unoesc.lan>
2013-04-14 14:27:47 -0300

    Pronto

--- EstruturaDeDados/src/Ordenacao/Teste.java
+++ EstruturaDeDados/src/Ordenacao/Teste.java
@@ -6,51 +6,74 @@ public class Teste {
         int tamanho = 1000;
         for (int i = 0; i < 7; i++) {
             teste(tamanho);
-            tamanho*=10;
+            tamanho *= 10;
         }
-        
     }
 
     private static void teste(int tamanho) {
         //BubbleSort
         char[] a = geraArray(tamanho);
         long t1 = System.currentTimeMillis();//Tempo inicial
-        BubbleSort.sort(a);
-        System.out.println("Tempo de execução do BulleSort para "
-                + tamanho + " itens: "
-                + (System.currentTimeMillis() - t1) / 1000 + " segundos.");
+        if (tamanho < 1000000) {
+            BubbleSort.sort(a);
+            System.out.println("Tempo de execução do BulleSort para "
+                    + tamanho + " itens: "
+                    + (System.currentTimeMillis() - t1) / 100 + " décimo de segundo.");
+        } else {
+            System.out.println("Tempo de execução do BulleSort para "
+                    + tamanho + " itens: maior que 10 minutos");
+        }
 
         //InsertSort
-        a = geraArray(tamanho);
-        t1 = System.currentTimeMillis();//Zera o tempo
-        InsertSort.sort(a);
-        System.out.println("Tempo de execução do InsertSort para "
-                + tamanho + " itens: "
-                + (System.currentTimeMillis() - t1) / 1000 + " segundos.");
+        if (tamanho < 10000000) {
+            a = geraArray(tamanho);
+            t1 = System.currentTimeMillis();//Zera o tempo
+            InsertSort.sort(a);
+            System.out.println("Tempo de execução do InsertSort para "
+                    + tamanho + " itens: "
+                    + (System.currentTimeMillis() - t1) / 100 + " décimo de segundo.");
+        } else {
+            System.out.println("Tempo de execução do InsertSort para "
+                    + tamanho + " itens: maior que 10 minutos");
+        }
 
         //SelectionSort
-        a = geraArray(tamanho);
-        t1 = System.currentTimeMillis();//Zera o tempo
-        SelectionSort.sort(a);
-        System.out.println("Tempo de execução do SelectionSort para "
-                + tamanho + " itens: "
-                + (System.currentTimeMillis() - t1) / 1000 + " segundos.");
+        if (tamanho < 1000000) {
+            a = geraArray(tamanho);
+            t1 = System.currentTimeMillis();//Zera o tempo
+            SelectionSort.sort(a);
+            System.out.println("Tempo de execução do SelectionSort para "
+                    + tamanho + " itens: "
+                    + (System.currentTimeMillis() - t1) / 100 + " décimo de segundo.");
+        } else {
+            System.out.println("Tempo de execução do SelectionSort para "
+                    + tamanho + " itens: maior que 10 minutos");
+        }
 
         //MergeSort
+        if(tamanho < 1000000){
         a = geraArray(tamanho);
         t1 = System.currentTimeMillis();//Zera o tempo
         MergeSort.sort(a);
         System.out.println("Tempo de execução do MergeSort para "
                 + tamanho + " itens: "
-                + (System.currentTimeMillis() - t1) / 1000 + " segundos.");
-
+                + (System.currentTimeMillis() - t1) / 100 + " décimo de segundo.");
+        }else{
+            System.out.println("Tempo de execução do MergeSort para "
+                + tamanho + " itens: Java heap space");
+        }
         //QuickSort
+        if(tamanho < 1000000){
         a = geraArray(tamanho);
         t1 = System.currentTimeMillis();//Zera o tempo
         QuickSort.sort(a);
         System.out.println("Tempo de execução do QuickSort para "
                 + tamanho + " itens: "
-                + (System.currentTimeMillis() - t1) / 1000 + " segundos.");
+                + (System.currentTimeMillis() - t1) / 100 + " décimo de segundo.");
+        }else{
+            System.out.println("Tempo de execução do QuickSort para "
+                + tamanho + " itens: StackOverflow");
+        }
     }
 
     private static char[] geraArray(int tamanho) {
